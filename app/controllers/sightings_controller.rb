40a06bd2890a592b3f8280f27ee1133d346d81e0
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 23:36:05 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -10,6 +10,6 @@ class SightingsController < ApplicationController
     options = {
       include: [:bird, :location]
     }
-    render json: SightingSerializer.new(sighting)
+    render json: SightingSerializer.new(sighting, options)
   end
 end
