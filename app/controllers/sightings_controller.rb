fda7fcf9ea100069ff659423b0bc660ca2bb1a9d
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 23:29:35 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,10 @@
 class SightingsController < ApplicationController
+  def index
+    sightings = Sighting.all
+    #render json: sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+    render json: SightingSerializer.new(sightings)
+  end
+
   def show
     sighting = Sighting.find(params[:id])
     #render json: sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
